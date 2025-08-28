f9a3dd2d3ea25301508276d7dfabd2a5229f75f5
TracerMeterMan <judethomaschaplin@gmail.com>
2025-08-28 08:06:58 -0600

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -6,10 +6,10 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return 'Bot is alive!'
+    return 'Bot is alive!', 200
 
 def run():
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))  # Render assigns PORT env
     app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
